a57c40b4f6deac9bb4123ebf07085d05fdf630e1
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-03-07 19:39:26 -0400

    Avances
    Se empezo a estrucuturara la consulta de tickets comprados y disponibles a la base de datos.

--- src/routes/Checking.jsx
+++ src/routes/Checking.jsx
@@ -11,22 +11,34 @@ const Checking = () => {
 
     const { userInfo } = useContext(context)
     const [openTicket, setOpenTicket] = useState(false)
+    const [showBuy, setShowBuy] = useState(false)
+    const [showCheck, setShowCheck] = useState(false)
     const [showTickets, setShowTickets] = useState([])
     let tickets = []
 
-    useEffect( () => {
-        console.log(userInfo.userId)
-        async () => {
-            const q = query(collection(db, "tickets"), where("comprador", "==", userInfo.userId));
 
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-            tickets = [...tickets, doc.data()]
-            
+    async function wannaCheck(){
+        setShowBuy(false)
+        setShowCheck(true)
+
+        const q = query(collection(db, "tickets"), where("comprador", "==", userInfo.userId));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+        tickets = [...tickets, doc.data()]
         });
-        }
-        setShowTickets(tickets)
-    }, [] )
+    }
+
+    async function wannaBuy(){
+        setShowBuy(true)
+        setShowCheck(false)
+
+        const q = query(collection(db, "tickets"), where("comprador", "==", userInfo.userId));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+        tickets = [...tickets, doc.data()]
+        });
+    }
+        
 
     async function openCloseTicket(){
         setOpenTicket(!openTicket)
@@ -35,10 +47,15 @@ const Checking = () => {
     return(
         <div className="Checking">
             <h1>bienvenido: {userInfo.name}</h1>
+            <div className='buttons'>
+                <Button variant='contained' onClick={wannaCheck} >ver tickets</Button>
+                <Button variant='contained' onClick={wannaBuy}>comprar</Button>
+            </div>
 
-            {showTickets.map( (ticket) => {
-                <h1>{ticket.comprador}</h1>
-            } )}
+            { showBuy && <></> }
+
+            { showCheck && <></> }
+
             { openTicket && <Ticket close={openCloseTicket}/> }
 
         </div>
