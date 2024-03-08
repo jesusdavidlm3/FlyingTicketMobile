147e9a729e2e198a7c7671c61968bc98cbb16f2a
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-03-08 13:10:58 -0400

    Avanaces
    Ya se puede obtener la lista de tickets disponibles, estilos aplicados. quitar anidado para consulta de los tickets comprados.

--- src/routes/Checking.jsx
+++ src/routes/Checking.jsx
@@ -23,20 +23,24 @@ const Checking = () => {
 
         const q = query(collection(db, "tickets"), where("comprador", "==", userInfo.userId));
         const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-        tickets = [...tickets, doc.data()]
+        querySnapshot.forEach(async (doc1) => {
+            const docRef = doc(db, "vuelos", doc1.data().vuelo);
+            const doc = await getDocFromCache(docRef);
+            tickets = [...tickets, doc.data()]
         });
+        setShowTickets(tickets)
     }
 
     async function wannaBuy(){
         setShowBuy(true)
         setShowCheck(false)
 
-        const q = query(collection(db, "tickets"), where("comprador", "==", userInfo.userId));
-        const querySnapshot = await getDocs(q);
+        const querySnapshot = await getDocs(collection(db, 'vuelos'));
         querySnapshot.forEach((doc) => {
         tickets = [...tickets, doc.data()]
+        console.log(doc.data())
         });
+        setShowTickets(tickets);
     }
         
 
@@ -46,15 +50,40 @@ const Checking = () => {
 
     return(
         <div className="Checking">
-            <h1>bienvenido: {userInfo.name}</h1>
+            <h1>Bienvenido: {userInfo.name}</h1>
             <div className='buttons'>
                 <Button variant='contained' onClick={wannaCheck} >ver tickets</Button>
                 <Button variant='contained' onClick={wannaBuy}>comprar</Button>
             </div>
 
-            { showBuy && <></> }
+            { showBuy && <div className='listContainer'>
+                { showTickets.map( (ticket) => (
+                    <div className='OneTicket'>
+                        <div className='info'>
+                            <p>Destino: {ticket.destino}</p>
+                            <p>Hora:</p>
+                            <p>Duracion: {ticket.duracion}</p>
+                        </div>
 
-            { showCheck && <></> }
+                        <Button variant='contained'>comprar</Button>
+                        {/* <h4>{ticket.hora.toLocaleDateString()}</h4> */}
+                    </div>
+                ) ) }
+            </div> }
+
+            { showCheck && <div className='list Container'>
+                { showTickets.map( (ticket) => (
+                    <div className='OneTicket'>
+                        <div className='info'>
+                            <p>Destino: {ticket.destino}</p>
+                            <p>Hora:</p>
+                            <p>Duracion: {ticket.duracion}</p>
+                        </div>
+
+                        <Button variant='contained'>Visualizar</Button>
+                    </div>
+                ) ) }
+            </div> }
 
             { openTicket && <Ticket close={openCloseTicket}/> }
 
